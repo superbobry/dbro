db977d12100b86a8ae64ef9cf8b661c005ce2df7
Sergei Lebedev <superbobry@gmail.com>
2012-11-27 01:36:04 +0400

    Fixed 'rowSize' calculation
      - Added data segment start byte
      - Swithed to 'Int64' for word size, since 'Data.Binary' seems to use
        it even on 32-bit platforms

--- src/Data/Bro/Backend/Util.hs
+++ src/Data/Bro/Backend/Util.hs
@@ -4,19 +4,26 @@ module Data.Bro.Backend.Util
   ( rowSize
   ) where
 
+import Data.Int (Int64)
 import Foreign.Storable (sizeOf)
 
 import Data.Bro.Types (ColumnType(..), TableSchema)
 
 rowSize :: TableSchema -> Int
 rowSize tabSchema = (+ overhead) $! sum $! do
+    -- FIXME(Sergei): WHY 'Data.Binary' uses 8-byte integers on
+    -- on 32-bit platforms?
     (_name, t) <- tabSchema
     return $! 1 + case t of  -- +1 for tag value.
         DoubleColumn    -> 25
         IntegerColumn   -> 4
-        VarcharColumn l -> fromIntegral l + (sizeOf (undefined :: Int))
+        VarcharColumn l -> fromIntegral l + word
   where
+    word :: Int
+    word = sizeOf (undefined :: Int64)
+
     overhead =
-        1 +                            -- maybe tag
-        (sizeOf (undefined :: Int)) +  -- row id
-        (sizeOf (undefined :: Int))    -- list length
+        1    +  -- data segment tag
+        1    +  -- row id maybe tag
+        word +  -- row id
+        word    -- list length
