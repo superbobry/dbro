a2324c8866280e739087b260cfcf3a60617e5116
Sergei Lebedev <superbobry@gmail.com>
2012-11-27 01:59:13 +0400

    'selectAll' now outputs CSV via 'cassava'

--- src/Data/Bro/Backend/Result.hs
+++ src/Data/Bro/Backend/Result.hs
@@ -5,10 +5,24 @@ module Data.Bro.Backend.Result
   , Result(..)
   ) where
 
+import Data.Csv (ToField(..), ToRecord(..))
+import qualified Data.Csv as Csv
 import qualified Data.ByteString.Char8 as S
+import qualified Data.ByteString.Lazy as L
+import qualified Data.Vector as V
 
-import Data.Bro.Types (Row, RowId)
+import Data.Bro.Types (Row(..), RowId, ColumnValue(..))
 import Data.Bro.Backend.Error (BackendError)
+
+instance ToField ColumnValue where
+    toField (IntegerValue i) = toField i
+    toField (DoubleValue d) = toField d
+    toField (VarcharValue s) = toField s
+    {-# INLINE toField #-}
+
+instance ToRecord Row where
+    toRecord = toRecord . rowData
+    {-# INLINE toRecord #-}
 
 class BackendResult r where
     format :: r -> S.ByteString
@@ -25,8 +39,9 @@ instance BackendResult RowId where
     format = S.pack . show
 
 instance BackendResult [Row] where
-    -- FIXME(Sergei): use CSV formatting here.
-    format = S.pack . show
+    -- FIXME(Sergei): switch to 'Data.Vector'?
+    -- TODO(Sergei): add column names and types.
+    format = S.concat . L.toChunks . Csv.encode . V.fromList
 
 instance BackendResult BackendError where
     format = S.pack . show
